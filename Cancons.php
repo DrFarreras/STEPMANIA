f6a2d3b2cdabc702acc8bdd016ed026b3f67e765
‘DrFarreras’ <gfarrerasperez@cendrassos.net>
2024-10-07 16:27:39 +0200

    añadir canciones al json

---
+++ Cancons.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+
+</head>
+<body>
+    
+    <div class="Botohome_classificacions">
+        <a href='index.html' class="buttonhome"></a><br>
+    </div>
+    
+    <div class="Llistacancons_cancons">
+        <ul class="song_list_ul">
+            
+            Llista de cançons<br><br>
+            
+        </ul>
+    
+        
+        
+    </div>
+    <div class="boto_foot_cancons"> 
+        <button class="btn-jugar-cancons">JUGAR</button>
+    </div>
+    
+</body>
+</html>
+
+<?php
+
+$json = file_get_contents("Data.json");
+$contents = json_decode($json, true);
+
+
+?>
+
+<html>
+    <body>
+        <?php foreach($contents as $Canco){ ?>
+            <p>
+        <img src="<?php echo $Canco["Caratula:"];?>" >
+        <?php echo $Canco["Titol:"]; ?>
+        </p>
+        <?php } ?>
+    </body>
+</html>
---
+++ Llistacancons.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">    <!-- Enllaçem amb index.css per fer canvis del php-->
+
+</head>
+<body>
+    <form action="Llistacancons.php" method="Post">     
+    
+    <div class="Botonshomereturn"> <!-- Fem un class dels botons enllaçats amb les seves pàgines corresponents-->
+
+        <a href='Afegircancons.php' class="buttonreturn"></a><br>
+        <a href='index.html' class="buttonhome"></a><br>
+
+    </div>
+
+    </form>
+</body>
+</html>
+
+<?php       /*Fem un php per poder fer que el formulario envii les dades al json*/
+$json = file_get_contents("data.json"); 
+$Cancons = json_decode($json, true); /*El arxiu converteix el contingut en un array php*/
+
+$next = count($Cancons) + 1; /*Conta quantes cancons hi ha i suma + 1 per preparar els noms dels nous arxius*/
+
+$Canco = [  /**Crea una array amb la nova informació enviada desde el formulari */
+    "ID" => uniqid(number_format(true)), /**fem una funció que genera una id */
+    "Titol:" => $_POST["titol"] ,
+    "Artista:" => $_POST["artista"],
+    "Caratula:" => "img{$next}.jpg",
+    "Joc:" => "joc{$next}.jpg",
+    "Canco:" => "canco{$next}.mp3"
+];
+    $Cancons[] = $Canco; /**S'afegeix la nova cancó de l'array cancó al array de cancons, actualitzant l'array final */
+
+    $json = json_encode($Cancons);
+    file_put_contents("data.json", $json); /**Guardem l'array actualitzat en data.json */
+    
+    if (move_uploaded_file($_FILES["fmusic"]["tmp_name"], "Uploads/canco/". $Canco["Canco:"]) && /**Moguem els arxius separats en les carpetes que pertanyin */
+    move_uploaded_file($_FILES["fcarat"]["tmp_name"], "Uploads/imatge/". $Canco["Caratula:"]) &&
+    move_uploaded_file($_FILES["fjoc"]["tmp_name"], "Uploads/joc/". $Canco["Joc:"]));
+
+
+?>
+
+
+
+
