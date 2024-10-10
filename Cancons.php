be5abebc742469bd8cd252ea00dacf5a1d5cca64
‘DrFarreras’ <gfarrerasperez@cendrassos.net>
2024-10-10 17:38:12 +0200

    cambios visuales en la parte de las canciones de la pagina jugar

--- Cancons.php
+++ Cancons.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,25 +8,34 @@
     <link rel="stylesheet" href="index.css">
 
 </head>
+
 <body>
-    
+
     <div class="Botohome_classificacions">
         <a href='index.html' class="buttonhome"></a><br>
     </div>
-    
+
     <div class="Llistacancons_cancons">
         <ul class="song_list_ul">
-            
-            Llista de cançons<br><br>
-            
+
+            Llista de cançons<br>
         </ul>
-    
-        
-        
     </div>
-    
-    
+    <div class="titols_lista_pantalla_jugar">
+        <div class="bloc_esquerra">
+            CARATULA
+        </div>
+        <div class="bloc_central">
+            TITOL
+        </div>
+        <div class="bloc_dreta">
+            ARTISTA
+        </div>
+
+    </div>
+
 </body>
+
 </html>
 
 <?php
@@ -37,19 +47,27 @@ $contents = json_decode($json, true);
 ?>
 
 <html>
-    <body>
-        <div class="lista_pantalla_jugar">
-            <?php foreach($contents as $Canco){ ?>
-                    <img class="caratula_canco_jugar"src="Uploads/imatge/<?php echo $Canco["Caratula:"];?>">
-                
-                <div class="cancons_pantalla_jugar">
-                    <?php echo $Canco["Titol:"];?><br><?php echo $Canco["Artista:"];?>
-                </div>                
-            <?php } ?>
-        </div>
-        <div class="boto_foot_cancons"> 
-            <button class="btn-jugar-cancons">JUGAR</button>
-        </div>
-    </body>
-</html>
 
+<body>
+    <div class="lista_pantalla_jugar">
+        <?php foreach ($contents as $Canco) { ?>
+
+            <div class="bloc_esquerra_caratula">
+                <img class="caratula_canco_jugar" src="Uploads/imatge/<?php echo $Canco["Caratula:"]; ?>">
+            </div>
+            <div class="bloc_central_titol">
+                <?php echo $Canco["Titol:"]; ?><br>
+            </div>
+            <div class="bloc_dreta_artista">
+                <?php echo $Canco["Artista:"]; ?>
+            </div>
+
+        <?php } ?>
+    </div>
+    <div class="boto_foot_cancons">
+        <button class="btn-jugar-cancons">JUGAR</button>
+    </div>
+</body>
+
+
+</html>
